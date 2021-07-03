e3913c3bb71b7c0346c608c933f0dc58241e50b3
kl4532 <kornellukasiak@gmail.com>
2021-07-03 08:03:03 +0200

    add favorites and download

--- src/app/components/gifs/detail/detail.component.ts
+++ src/app/components/gifs/detail/detail.component.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {GifsService} from "../../../services/gifs.service";
 import {Subscription} from "rxjs";
 import {Gif} from "../../../shared/models/gif.model";
+import {FavoritesService} from "../../../services/favorites.service";
+import {saveAs} from "file-saver";
 
 @Component({
   selector: 'app-gif-detail',
@@ -16,7 +18,8 @@ export class GifComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute,
               private router: Router,
-              private gifService: GifsService) { }
+              private gifService: GifsService,
+              private favoritesService: FavoritesService) { }
 
   ngOnInit(): void {
     this.gifId = this.activatedRoute.snapshot.paramMap.get('id');
@@ -28,7 +31,17 @@ export class GifComponent implements OnInit {
   }
 
   addToFavourites() {
-    console.log(this.gifId);
+    if(this.gif) {
+      this.favoritesService.addToFavorites(this.gif);
+    }
+  }
+
+  download() {
+    if(this.gif) {
+      this.favoritesService.downloadGif(this.gif).subscribe((blob: Blob) => {
+        saveAs(blob, `${this.gif?.title}.gif`);
+      });
+    }
   }
 
   ngOnDestroy(): void {
