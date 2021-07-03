92c63b9e50dd36abd962f632008acf7fc954578c
kl4532 <kornellukasiak@gmail.com>
2021-07-03 08:03:55 +0200

    fill favorites with data

--- src/app/components/favourites/favourites.component.ts
+++ src/app/components/favourites/favourites.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {FavoritesService} from "../../services/favorites.service";
+import {Gif} from "../../shared/models/gif.model";
+import {saveAs} from "file-saver";
 
 @Component({
   selector: 'app-favourites',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favourites.component.scss']
 })
 export class FavouritesComponent implements OnInit {
+  gifs: Gif[] | undefined;
 
-  constructor() { }
+  constructor(private favoritesService: FavoritesService) { }
 
   ngOnInit(): void {
+    this.favoritesService.changesSubject.subscribe(()=> this.gifs = this.favoritesService.getFromFavorites());
   }
 
+  removeFromFavorites(id: string) {
+    this.favoritesService.removeFromFavorites(id);
+  }
+
+  download(id: string) {
+      const gif = this.gifs?.find(gif => gif.id === id);
+      if(gif) {
+        this.favoritesService.downloadGif(gif).subscribe((blob: Blob) => {
+          saveAs(blob, `${gif.title}.gif`);
+        });
+      }
+  }
+
+  share() {
+
+  }
 }
