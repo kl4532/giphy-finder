e157f4143b7aa65e7ecc0eb702b88b5a5780878e
kl4532 <kornellukasiak@gmail.com>
2021-07-04 10:46:40 +0200

    final touches

--- src/app/components/gifs/detail/detail.component.spec.ts
+++ src/app/components/gifs/detail/detail.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {GifComponent} from "./detail.component";
 
-import { DetailComponent } from './detail.component';
 
 describe('DetailComponent', () => {
-  let component: DetailComponent;
-  let fixture: ComponentFixture<DetailComponent>;
+  let component: GifComponent;
+  let fixture: ComponentFixture<GifComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
+      declarations: [ GifComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailComponent);
+    fixture = TestBed.createComponent(GifComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
