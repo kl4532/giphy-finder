a682e477d30bfb803b1d9954ff1313f650639384
kl4532 <kornellukasiak@gmail.com>
2021-07-16 19:08:02 +0200

    no, it works :)

--- upContainer.sh
+++ upContainer.sh
@@ -2,8 +2,6 @@
 
 docker stop $1
 docker rm $1
-#docker image prune -a
-# image need to be deleted, pull doesnt work
-# run docker rmi <imgId>
+# optional - run docker rmi <imgId>
 docker pull $3/$2
 docker run -p 8080:80 --name $1 $3/$2
